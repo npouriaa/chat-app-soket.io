4ea50859dfed0406ee959ba97e98f8ac65978461
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-10-26 16:35:32 +0330

    connecting backend and frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import './App.css'
+import io from 'socket.io-client'
+const socket =  io.connect('http://localhost:5174')
 
 const App = () => {
+  console.log(socket)
   return (
     <div>App</div>
   )
