16ef3511af4e8095e0c3997daa2d12a13d926503
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-10-26 17:32:20 +0330

    add style : add bg and send input

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
-import './App.css'
-import io from 'socket.io-client'
-const socket =  io.connect('http://localhost:5174')
+import React from "react";
+import "./App.css";
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:5174");
 
 const App = () => {
-  console.log(socket)
   return (
-    <div>App</div>
-  )
-}
+    <div className="container">
+      <form className="message-input-con" action="">
+        <input
+          onChange={(e) => setText(e.target.value)}
+          type="text"
+          placeholder="Message"
+        />
+        <button type="submit">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+            />
+          </svg>
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default App
+export default App;
